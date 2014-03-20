6d400d57d79ed00816b1a04499c8813eb74a1f9e
Uros Djunisijevic <bro4real@live.com>
2014-03-20 10:42:21 +0100

    homework unit 7

--- WallStApp/src/com/brothers4real/wallstapp/TestWallStreetApp.java
+++ WallStApp/src/com/brothers4real/wallstapp/TestWallStreetApp.java
@@ -7,11 +7,16 @@ import javax.swing.JPanel;
 public class TestWallStreetApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame mainFrame = new JFrame("STOCKBROKER");
+		JPanel container = new JPanel();
+		
+		// this constructor's parameters: name of the stock, 
+		// its minimal price, and price range
 		ActionPane action1 = new ActionPane("AAPL", 500, 100);
 		
-		JPanel container = new JPanel();
-		JFrame mainFrame = new JFrame("STOCKBROKER");
-		
+		// this constructor's parameters: JFrame, JPanel, 
+		// ActionPane, window's width and height
 		Display BrokersHelper = new Display(mainFrame, container, action1, 500, 300);
 		Timer t = new Timer();
 		t.schedule(BrokersHelper, 0, 1000);
